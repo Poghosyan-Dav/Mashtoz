3ebe9e962c52123509ab49eae6b632c06b0cfc53
Poghosyan-Dav <poghosyandav3@gmail.com>
2023-06-18 10:46:27 -0700

    update app version 3

--- lib/ui/widgets/main_page/main_menu_pages/italian_lesson/italian_data_show.dart
+++ lib/ui/widgets/main_page/main_menu_pages/italian_lesson/italian_data_show.dart
@@ -23,7 +23,10 @@ class ItaliaLessonShow extends StatefulWidget {
 
   @override
   State<ItaliaLessonShow> createState() => ItaliaLessonShowState(
-      lessons: lessons, isShow: isShow, idLessons: idLessons);
+    lessons: lessons,
+    isShow: isShow,
+    idLessons: idLessons,
+  );
 }
 
 class ItaliaLessonShowState extends State<ItaliaLessonShow> {
@@ -34,6 +37,7 @@ class ItaliaLessonShowState extends State<ItaliaLessonShow> {
   bool isShowingDialog = false;
 
   ItaliaLessonShowState({this.lessons, this.isShow, this.idLessons});
+
   @override
   void initState() {
     userDataProvider.fetchUserInfo().then((value) => custemerId = value?.id);
@@ -52,207 +56,169 @@ class ItaliaLessonShowState extends State<ItaliaLessonShow> {
 
   final userDataProvider = UserDataProvider();
   final bookDataProvider = BookDataProvider();
+
   @override
   Widget build(BuildContext context) {
-    final orentation = MediaQuery.of(context).orientation;
-
+    final orientation = MediaQuery.of(context).orientation;
 
     return lessons != null
         ? Scaffold(
-            backgroundColor: Palette.textLineOrBackGroundColor,
-            body: Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20.0),
-              child: CustomScrollView(
+      backgroundColor: Palette.textLineOrBackGroundColor,
+      body: Padding(
+        padding: const EdgeInsets.only(right: 20, left: 20.0),
+        child: CustomScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          slivers: [
+            orientation != Orientation.landscape
+                ? SliverAppBar(
+              floating: false,
+              pinned: true,
+              leading: Container(
+                width: 7,
+                height: 14,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_outlined,
+                    color: Palette.appBarTitleColor,
+                  ),
+                  padding: const EdgeInsets.only(right: double.infinity),
+                  alignment: Alignment.center,
+                ),
+              ),
+              expandedHeight: 77,
+              backgroundColor: Palette.textLineOrBackGroundColor,
+              elevation: 0,
+              automaticallyImplyLeading: false,
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Color.fromRGBO(25, 4, 18, 1),
+              ),
+              actions: const [
+                MenuShow(),
+              ],
+            )
+                : const SliverToBoxAdapter(),
+            SliverToBoxAdapter(
+              child: SingleChildScrollView(
                 physics: const AlwaysScrollableScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                slivers: [
-                  orentation != Orientation.landscape
-                      ? SliverAppBar(
-                          // title: Transform(
-                          //   transform:
-                          //       Matrix4.translationValues(-40.0, 0.0, 0.0),
-                          //   child: Text(
-                          //     '${lessons?.number}',
-                          //     style: TextStyle(
-                          //         fontSize: 16,
-                          //         letterSpacing: 1,
-                          //         fontFamily: 'GHEAGrapalat',
-                          //         fontWeight: FontWeight.w700,
-                          //         color: Palette.appBarTitleColor),
-                          //   ),
-                          // ),
-                          floating: false,
-                          pinned: true,
-                          leading: Container(
-                            width: 7,
-                            height: 14,
-                            child: IconButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              //iconSize: 13,
-                              icon: const Icon(
-                                Icons.arrow_back_ios_new_outlined,
-                                color: Palette.appBarTitleColor,
+                child: Column(
+                  children: [
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context, rootNavigator: true).push(
+                            MaterialPageRoute(
+                              builder: (context) => VideoView(
+                                link: lessons!.link!,
                               ),
-                              padding: const EdgeInsets.only(right: double.infinity),
-                              alignment: Alignment.center,
                             ),
-                          ),
-                          expandedHeight: 77,
-                          backgroundColor: Palette.textLineOrBackGroundColor,
-                          elevation: 0,
-                          automaticallyImplyLeading: false,
-                          systemOverlayStyle: const SystemUiOverlayStyle(
-                              statusBarColor: Color.fromRGBO(25, 4, 18, 1)),
-                          actions: [
-                            const MenuShow(),
+                          );
+                        },
+                        child: Stack(
+                          children: [
+                            CachedNetworkImage(
+                              imageUrl: lessons!.image!,
+                            ),
+                            const Positioned.fill(
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Center(
+                                  child: Icon(
+                                    Icons.play_arrow,
+                                    color: Colors.white,
+                                    size: 70,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ],
-                        )
-                      : const SliverToBoxAdapter(),
-                  SliverToBoxAdapter(
-                    child: SingleChildScrollView(
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.of(context, rootNavigator: true)
-                                    .push(MaterialPageRoute(
-                                        builder: (context) => VideoView(
-                                                  link: lessons!.link!,
-                                            )));
-                              },
-                              child: Stack(
-                                children: [
-                                  CachedNetworkImage(
-                                    imageUrl: lessons!.image!,
-                                  ),
-                                  const Positioned.fill(
-                                    child: Align(
-                                      alignment: Alignment.center,
-                                      child: Center(
-                                        child: Icon(
-                                          Icons.play_arrow,
-                                          color: Colors.white,
-                                          size: 70,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            child: Flexible(
-                              child: Text(
-                                lessons!.title ?? '',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 12.0,
-                                  fontFamily: 'GHEAGrapalat',
-                                  letterSpacing: 1,
-                                  color: Color.fromRGBO(84, 112, 126, 1),
-                                ),
-                                textAlign: TextAlign.start,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 15.0),
-                          const Divider(),
-                          orentation != Orientation.landscape
-                              ? Container(
-                                  color: Palette.textLineOrBackGroundColor,
-                                  width: double.infinity,
-                                  height: 49,
-                                  child: Row(
-                                    children: [
-                                      InkWell(
-                                        onTap: () async {
-                                          await Share.share(lessons!.link!);
-                                          // print('kisvel');
-
-                                          // showDialog(
-                                          //     context: context,
-                                          //     barrierDismissible: true,
-                                          //     builder: (
-                                          //       context,
-                                          //     ) =>
-                                          //         SaveShowDialog(
-                                          //           isShow: false,
-                                          //         ));
-                                        },
-                                        child: Row(
-                                          children: [
-                                            //  const SizedBox(width: 16),
-                                            SvgPicture.asset(
-                                                'assets/images/այքըններ.svg'),
-                                            const SizedBox(width: 6),
-                                            const Text('Կիսվել')
-                                          ],
-                                        ),
-                                      ),
-                                      const Spacer(),
-                                      InkWell(
-                                        onTap: () {
-
-
-                                            userIsSign(context);
-
-
-
-                                        },
-                                        child: Row(
-                                          children: [
-                                            SvgPicture.asset(
-                                                'assets/images/վելացնել1.svg'),
-                                            const SizedBox(width: 6),
-                                            const Text('Պահել'),
-                                            //const SizedBox(width: 16),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ))
-                              : const SizedBox(
-                                  height: 0.1,
-                                ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        lessons!.title ?? '',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12.0,
+                          fontFamily: 'GHEAGrapalat',
+                          letterSpacing: 1,
+                          color: Color.fromRGBO(84, 112, 126, 1),
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    const Divider(),
+                    orientation != Orientation.landscape
+                        ? Container(
+                      color: Palette.textLineOrBackGroundColor,
+                      width: double.infinity,
+                      height: 49,
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () async {
+                              await Share.share(lessons!.link!);
+                            },
+                            child: Row(
+                              children: [
+                                SvgPicture.asset('assets/images/այքըններ.svg'),
+                                const SizedBox(width: 6),
+                                const Text('Կիսվել'),
+                              ],
+                            ),
+                          ),
+                          const Spacer(),
+                          InkWell(
+                            onTap: () {
+                              userIsSign(context);
+                            },
+                            child: Row(
+                              children: [
+                                SvgPicture.asset('assets/images/վելացնել1.svg'),
+                                const SizedBox(width: 6),
+                                const Text('Պահել'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                        : const SizedBox(height: 0.1),
+                  ],
+                ),
               ),
             ),
-          )
+          ],
+        ),
+      ),
+    )
         : const Center(
-          child: CircularProgressIndicator(
-            color: Palette.main,
-          ),
-        );
+      child: CircularProgressIndicator(
+        color: Palette.main,
+      ),
+    );
   }
 
   void userIsSign(BuildContext context) async {
-    final tabProvider = Provider.of<TabProvider>(context,listen: false);
+    final tabProvider = Provider.of<TabProvider>(context, listen: false);
 
-    var data = <String,dynamic>{};
-    await   userDataProvider.fetchUserInfo().then((value) {
-
-      data = <String,dynamic>{
-        'type':
-        'lessons',
-        'type_id':
-    lessons?.id,
-        'customer_id':
-        value?.id,
+    var data = <String, dynamic>{};
+    await userDataProvider.fetchUserInfo().then((value) {
+      data = <String, dynamic>{
+        'type': 'lessons',
+        'type_id': lessons?.id,
+        'customer_id': value?.id,
       };
     });
-    if(data.isNotEmpty){
-      tabProvider.updateSaveData(data,context);
+    if (data.isNotEmpty) {
+      tabProvider.updateSaveData(data, context);
       if (!isShowingDialog) {
         isShowingDialog = true;
         showDialog(
